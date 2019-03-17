a230522580e6ec85837fdd5bd902d914bff0d768
dhruvshah57 <dhruvshah57@gmail.com>
2019-03-17 03:57:10 -0700

    POC
    Gun with Raycast to other object and instantiatibg PS

---
+++ SpaceAR Game/Assets/WR/Script/DeclarationManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclarationManager : MonoBehaviour
+{
+    public static DeclarationManager isn { set; get; }
+    public Transform CrossHair_tra;
+    public ParticleSystem HitParticleEffect;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        isn = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SpaceAR Game/Assets/WR/Script/Shooting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    private RaycastHit hit;
+    private DeclarationManager DM;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DM = DeclarationManager.isn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Perform Muzzle Flash
+            DORayCast();
+        }
+    }
+
+    void DORayCast()
+    {
+        if( Physics.Raycast(DM.CrossHair_tra.transform.position, DM.CrossHair_tra.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            //var main =HitParticleEffect.main;
+            GameObject go = Instantiate(DM.HitParticleEffect.gameObject);
+            go.transform.position = hit.point;
+            //Deal Damage To Object 
+        }
+    }
+}
