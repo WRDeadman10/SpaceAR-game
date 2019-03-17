30a2ea065e98d6396cbf5ca06da847abdd4dc69c
dhruvshah57 <dhruvshah57@gmail.com>
2019-03-17 06:16:24 -0700

    POC
    POC with Plane instatiate at Anchor

--- SpaceAR Game/Assets/WR/Script/DeclarationManager.cs
+++ SpaceAR Game/Assets/WR/Script/DeclarationManager.cs
@@ -7,6 +7,8 @@ public class DeclarationManager : MonoBehaviour
     public static DeclarationManager isn { set; get; }
     public Transform CrossHair_tra;
     public ParticleSystem HitParticleEffect;
+    public GameObject EnemyPlane;
+    public List<Transform> Enemypos;
     // Start is called before the first frame update
     void Awake()
     {
--- SpaceAR Game/Assets/WR/Script/Shooting.cs
+++ SpaceAR Game/Assets/WR/Script/Shooting.cs
@@ -1,22 +1,45 @@
-﻿using System.Collections;
+﻿using GoogleARCore;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
 
-    private RaycastHit hit;
-    private DeclarationManager DM;
+    private RaycastHit hit1;
     private bool Toggle;
+    private List<DetectedPlane> m_AllPlanes = new List<DetectedPlane>();
+
     // Start is called before the first frame update
     void Start()
     {
-        DM = DeclarationManager.isn;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void InsPlanes(TrackableHit hit)
     {
+        // Choose the Andy model for the Trackable that got hit.
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject prefab;
+            prefab = DeclarationManager.isn.EnemyPlane;
+
+            // Instantiate Andy model at the hit pose.
+            var EnemyPlane = Instantiate(prefab, DeclarationManager.isn.Enemypos[i].position, Quaternion.identity);
+
+            // Compensate for the hitPose rotation facing away from the raycast (i.e. camera).
+            EnemyPlane.transform.Rotate(0, 180, 0, Space.Self);
+
+            // Create an anchor to allow ARCore to track the hitpoint as understanding of the physical
+            // world evolves.
+            var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+            // Make Andy model a child of the anchor.
+            EnemyPlane.transform.parent = anchor.transform;
+        }
+
+
+        // Update is called once per frame
+
         if (Input.GetMouseButtonDown(0) && !Toggle)
         {
             // Perform Muzzle Flash
@@ -32,14 +55,14 @@ public class Shooting : MonoBehaviour
         }
 
     }
-
+   
     void DORayCast()
     {
-        if( Physics.Raycast(DM.CrossHair_tra.transform.position, DM.CrossHair_tra.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        if (Physics.Raycast(DeclarationManager.isn.CrossHair_tra.transform.position, DeclarationManager.isn.CrossHair_tra.TransformDirection(Vector3.forward), out hit1, Mathf.Infinity))
         {
             //var main =HitParticleEffect.main;
-            GameObject go = Instantiate(DM.HitParticleEffect.gameObject);
-            go.transform.position = hit.point;
+            GameObject go = Instantiate(DeclarationManager.isn.HitParticleEffect.gameObject);
+            go.transform.position = hit1.point;
             //Deal Damage To Object 
         }
     }
